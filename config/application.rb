15b403d71e17ddbe7ee66d17742921b132c60f72
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2023-02-25 07:20:06 +0000

    Redisの設定

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,11 @@ module App
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
 
+    config.cache_store = :redis_cache_store, {
+      url: ENV.fetch('REDIS_URL', 'redis://redis:6379/1'),
+      namespace: 'cache'
+    }
+
     config.generators.system_tests = nil
     config.generators do |g|
       g.assets false
