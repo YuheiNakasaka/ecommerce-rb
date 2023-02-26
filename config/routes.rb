6b88a039f233f3a016ddd569f85cd258f22bfd3d
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2023-02-26 01:20:30 +0000

    Deviseで認証機能を作る

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions'
+  }
+  devise_for :customers, controllers: {
+    sessions: 'customer/sessions',
+    registrations: 'customer/registrations'
+  }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'pages#home'
 end
