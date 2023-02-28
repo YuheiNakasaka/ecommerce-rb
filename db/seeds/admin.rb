3b735f8f6859b56e932eb607c7238a5a3b8e9649
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2023-02-28 04:40:51 +0000

    ユーザーのSeed生成設定

---
+++ db/seeds/admin.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+Admin.create!(email: 'admin@example.com', password: 'pppppppp')
---
+++ db/seeds/customer.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+Customer.create!(email: 'guest001@example.com', password: 'pppppppp', name: '検証太郎')
