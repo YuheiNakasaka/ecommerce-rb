bfc5210ea281d421834a991746f27c30a872cd9a
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2023-03-04 08:36:48 +0000

    カートに関する実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,6 +10,8 @@ class Product < ApplicationRecord
   end
 
   has_one_attached :image
+  has_many :cart_items, dependent: :destroy
+
   scope :price_high_to_low, -> { order(price: :desc) }
   scope :price_low_to_high, -> { order(price: :asc) }
 end
